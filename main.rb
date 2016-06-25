6af3431cfdb22ca5ef07c9521a6e7cafda506ab2
Charmel Sippio <charmelsippio@gmail.com>
2016-06-25 16:12:16 -0400

    charmel changes

--- main.rb
+++ main.rb
@@ -9,14 +9,22 @@ get '/about' do
   erb :about
 end
 
-get '/about' do
-  erb :about
-end
-
 get '/services' do
   erb :services
 end
 
-get '/' do
+get '/contact' do
   erb :contact
 end
+
+post '/email_response' do
+	puts params.inspect
+end
+
+mail_person = SendGrid::Client.new do |mp|
+	mp.api_key = ENV["SENDGRID_APIKEY"] #input the API key from SendGrid
+end
+
+res = client.send(mail)
+puts res.code
+puts res.body
