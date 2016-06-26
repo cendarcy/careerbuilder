15eb665936ca59d081ce7c04aba504a4baa136e9
Camille Davis <camilleedavis@gmail.com>
2016-06-26 17:24:08 -0400

    pulling requests

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 require "sinatra"
-require "sendgrid-ruby"
+require "postmark"
 
 get '/' do
   erb :index
@@ -21,9 +21,18 @@ post '/email_response' do
 	puts params.inspect
 end
 
-mail_person = SendGrid::Client.new do |mp|
-	mp.api_key = ENV["SENDGRID_APIKEY"] #input the API key from SendGrid
-end
+# Camille: would like to chat with Orlando/
+# TAs about how we could make this a working form
+
+# client = Postmark::ApiClient.new('server token')
+
+# client.deliver(
+#   from: 'sender@example.com',
+#   to: 'gets.chomp',
+#   subject: 'hello',
+#   html_body: '<strong>Hello from Postmark!<strong>',
+#   track_opens: true
+# )
 
 # res = client.send(mail)
 # puts res.code
