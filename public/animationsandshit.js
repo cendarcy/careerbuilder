bb9e011a919112ac9f93ad5b1ee946653385090b
Charmel Sippio <charmelsippio@gmail.com>
2016-06-26 16:57:32 -0400

    updated WORKING js code

--- public/animationsandshit.js
+++ public/animationsandshit.js
@@ -1,46 +1,43 @@
-// var $animations = $(".animation-element");
-// var $window = $(window);
-
-// function if_inView() {
-// 	var window_height = $window.height();
-// 	var window_top = $window.scrollTop();
-// 	var window_bottom = (window_top + window_height);
-
-// 	$.each($animations, function() {
-// 		var $element = $(this);
-// 		var element_height = $element.outerHeight();
-// 		var element_top = $element.offset().top;
-// 		var element_bottom = (element_top + element_height);
-
-// 		if ((element_bottom >= window_top) && (element_top <= window_bottom)) {
-// 			$element.addClass("in-view");
-// 		} else {
-// 			$element.removeClass("in-view");
-// 		}
-// 	});
-// }
-
-// // $window.on("scroll resize", if_inView);
-// $window.trigger("scroll");
-
 $(window).scroll(function(){
   var windowWidth = $(this).width();
   var windowHeight = $(this).height();
   var windowScrollTop = $(this).scrollTop();
 
 var pictureSlide = function() {
-	$('.asianW').delay(2000).animate({
+	$('.asianW').animate({
 		opacity: 1,
-		left: 0}, 
+		left: 12}, 
 		'slow');
 	
-	$('#asianwoman').find('img').delay(2000).animate({
+	$('.asianwoman').animate({
 		opacity: 1,
-		right: 0},
+		right: 20},
+		'slow');
+
+	$('.latinaW').delay(2000).animate({
+	opacity: 1,
+	right: 12}, 
+	'slow');
+	
+	$('.latinawoman').delay(2000).animate({
+		opacity: 1,
+		left: 20},
 		'slow');
 };
 
-if(windowScrollTop > 200) {
-	$('.asianW').css();
+if(windowScrollTop >= 200) {
+	$('.asianW', '.asianwoman');
+};
+if(windowScrollTop >= 900) {
+	$('.latinaW', '.latinawoman');
 	pictureSlide();
-}
+	};
+});
+
+
+
+
+
+
+
+
