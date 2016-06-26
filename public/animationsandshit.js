632e5a976e6b638fc368eb4fb680bb77f8d35938
Camille Davis <camilleedavis@gmail.com>
2016-06-26 17:25:39 -0400

    let's see again

--- public/animationsandshit.js
+++ public/animationsandshit.js
@@ -1,27 +1,4 @@
-// var $animations = $(".animation-element");
-// var $window = $(window);
 
-// function if_inView() {
-// 	var window_height = $window.height();
-// 	var window_top = $window.scrollTop();
-// 	var window_bottom = (window_top + window_height);
-
-// 	$.each($animations, function() {
-// 		var $element = $(this);
-// 		var element_height = $element.outerHeight();
-// 		var element_top = $element.offset().top;
-// 		var element_bottom = (element_top + element_height);
-
-// 		if ((element_bottom >= window_top) && (element_top <= window_bottom)) {
-// 			$element.addClass("in-view");
-// 		} else {
-// 			$element.removeClass("in-view");
-// 		}
-// 	});
-// }
-
-// // $window.on("scroll resize", if_inView);
-// $window.trigger("scroll");
 
 $(window).scroll(function(){
   var windowWidth = $(this).width();
