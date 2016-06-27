8fa65b5e89b95cb65ee78c8852b7f4085aeaf732
Charmel Sippio <charmelsippio@gmail.com>
2016-06-27 00:19:16 -0400

    "updated code"

--- public/animationsandshit.js
+++ public/animationsandshit.js
@@ -5,29 +5,29 @@ $(window).scroll(function(){
   var windowScrollTop = $(this).scrollTop();
 
 var pictureSlide = function() {
-	$('.asianW').animate({
+	$('.asianW').delay(1000).animate({
 		opacity: 1,
 		left: 12}, 
 		'slow');
 	
-	$('.asianwoman').animate({
+	$('.asianwoman').delay(1000).animate({
 		opacity: 1,
 		right: 20},
 		'slow');
 
-	$('.latinaW').delay(2000).animate({
+	$('.latinaW').delay(2600).animate({
 	opacity: 1,
 	right: 12}, 
 	'slow');
 	
-	$('.latinawoman').delay(2000).animate({
+	$('.latinawoman').delay(2600).animate({
 		opacity: 1,
 		left: 20},
 		'slow');
 
 	$('.workshop').animate({
 		opacity: 1,
-		left: 20}, 
+		right: 12}, 
 		'slow');
 
 	$('.wwFG').animate({
@@ -35,34 +35,31 @@ var pictureSlide = function() {
 		left: 20}, 
 		'slow');
 
-	$('.laughing').animate({
+	$('.laughing').delay(1600).animate({
+		opacity: 1,
+		right: 12}, 
+		'slow');
+
+	$('.lwFG').delay(1600).animate({
 		opacity: 1,
 		left: 20}, 
 		'slow');
 
-	$('.lwFG').animate({
+	$('.group').delay(2600).animate({
 		opacity: 1,
-		left: 20}, 
+		left: 12}, 
 		'slow');
 
-	$('.group').animate({
+	$('.groupFG').delay(2600).animate({
 		opacity: 1,
 		right: 20}, 
 		'slow');
-
-	$('.groupFG').animate({
-		opacity: 1,
-		left: 20}, 
-		'slow');
 };
 
-if(windowScrollTop >= 200) {
+if(windowScrollTop >= 300) {
 	$('.asianW', '.asianwoman');
 };
-if(windowScrollTop >= 900) {
-	$('.latinaW', '.latinawoman');
-	pictureSlide();
-	};
+
 if(windowScrollTop >= 900) {
 	$('.latinaW', '.latinawoman');
 	pictureSlide();
@@ -71,11 +68,11 @@ if(windowScrollTop >= 300) {
 	$('.workshop', '.wwFG');
 	pictureSlide();
 	};
-if(windowScrollTop >= 900) {
+if(windowScrollTop >= 1000) {
 	$('.laughing', '.lwFG');
 	pictureSlide();
 	};	
-if(windowScrollTop >= 900) {
+if(windowScrollTop >= 1200) {
 	$('.group', '.lwFG');
 	pictureSlide();
 	};	
